385c8a2576fbe13aa0ed15ec5c485fedd98af93d
Nitish <nitishkumarsingh824@gmail.com>
2023-09-02 20:03:56 +0530

    added html pages

--- core/models.py
+++ core/models.py
@@ -1,3 +1,7 @@
+from django.db.models.signals import post_save
+from django.conf import settings
 from django.db import models
+from django.db.models import Sum
+from django.shortcuts import reverse
 
 # Create your models here.
